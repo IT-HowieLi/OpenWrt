1cd4fffea881b84c578c714514c5ff90c2d67b28
HowieLi <chinahowieli@gmail.com>
2020-04-27 12:49:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,14 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+
+# Modify Host Name
+sed -i '/uci commit system/i uci set system.@system[0].hostname=HowieLi'  package/lean/default-settings/files/zzz-default-settings
+
+# Modify Default Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Update Luci theme argon  
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
