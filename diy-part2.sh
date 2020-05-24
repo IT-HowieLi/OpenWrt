76ef55f22fabbf06e669c1d1982ab33ef288f462
HowieLi <chinahowieli@gmail.com>
2020-05-24 13:14:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.10/g' package/base-files/files/bin/config_generate
 
 # Modify Host Name
 # sed -i '/uci commit system/i uci set system.@system[0].hostname=HowieLi-OpenWrt'  package/lean/default-settings/files/zzz-default-settings
